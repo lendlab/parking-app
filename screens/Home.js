82beebb0fab3132e52e27e39078428f4bbea2593
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-12 18:18:32 -0300

    Create Home

---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#181818",
+        paddingHorizontal: 22,
+      }}
+    >
+      <Text style={{ color: "white" }}>Hola</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
