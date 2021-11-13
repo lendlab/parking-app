503150dc9b91565b037b48316532345b0357018d
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 08:12:47 -0300

    Add TouchableWithoutFeedback

--- components/ParkItem.js
+++ components/ParkItem.js
@@ -1,71 +1,85 @@
 import React from "react";
-import { View } from "react-native";
+import {
+  TouchableHighlightBase,
+  TouchableOpacityBase,
+  View,
+} from "react-native";
 import { Image, Text } from "react-native-elements";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 const ParkItem = () => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate("Map", { title: "Parque nashee" });
+  };
+
   return (
-    <View
-      style={{
-        height: 120,
-        backgroundColor: "#202020",
-        borderWidth: 1,
-        borderRadius: 8,
-        borderColor: "#2a2a2a",
-        marginTop: 18,
-        flexDirection: "row",
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://okdiario.com/img/2018/01/25/parking.jpg",
-        }}
-        style={{
-          width: 120,
-          height: 120,
-          borderBottomRightRadius: 8,
-          borderTopRightRadius: 8,
-        }}
-      />
+    <TouchableWithoutFeedback onPress={handlePress}>
       <View
         style={{
-          padding: 12,
-          flex: 1,
-          marginLeft: 8,
+          height: 120,
+          backgroundColor: "#202020",
+          borderWidth: 1,
+          borderRadius: 8,
+          borderColor: "#2a2a2a",
+          marginTop: 18,
+          flexDirection: "row",
         }}
       >
-        <Text
-          style={{
-            color: "#fff",
-            fontWeight: "bold",
+        <Image
+          source={{
+            uri: "https://okdiario.com/img/2018/01/25/parking.jpg",
           }}
-        ></Text>
-        <Text
           style={{
-            color: "#fff",
-            fontWeight: "bold",
-            marginBottom: 12,
+            width: 120,
+            height: 120,
+            borderBottomRightRadius: 8,
+            borderTopRightRadius: 8,
           }}
-        >
-          PARKING GODEANO
-        </Text>
+        />
         <View
           style={{
-            flexDirection: "row",
+            padding: 12,
+            flex: 1,
+            marginLeft: 8,
           }}
         >
-          <Feather name="clock" size={18} color="#fff" />
           <Text
             style={{
               color: "#fff",
-              marginLeft: 8,
+              fontWeight: "bold",
+            }}
+          ></Text>
+          <Text
+            style={{
+              color: "#fff",
+              fontWeight: "bold",
+              marginBottom: 12,
             }}
           >
-            34 minutos
+            PARKING GODEANO
           </Text>
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <Feather name="clock" size={18} color="#fff" />
+            <Text
+              style={{
+                color: "#fff",
+                marginLeft: 8,
+              }}
+            >
+              34 minutos
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
