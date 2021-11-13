51e49df5cf2232c3fd764511cc2a1f1f03ee4eaa
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 08:14:22 -0300

    Add styles to StyleSheet

--- screens/Parkings.js
+++ screens/Parkings.js
@@ -9,33 +9,9 @@ const Parkings = () => {
   return (
     <>
       <StatusBar backgroundColor="#202020" barStyle="light-content" />
-      <ScrollView
-        style={{
-          flex: 1,
-          backgroundColor: "#181818",
-        }}
-      >
-        <View
-          style={{
-            height: 220,
-            backgroundColor: "#202020",
-            borderBottomWidth: 1,
-            borderBottomEndRadius: 8,
-            borderBottomStartRadius: 8,
-            borderColor: "#2a2a2a",
-            paddingHorizontal: 22,
-            justifyContent: "center",
-          }}
-        >
-          <Text
-            h1
-            h1Style={{
-              color: "#FFF",
-              fontSize: 42,
-              fontFamily: "Inter_900Black",
-              textAlign: "center",
-            }}
-          >
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <Text h1 h1Style={styles.title}>
             Parkings
           </Text>
           <Input placeholder="Busca un parking..." />
@@ -48,4 +24,25 @@ const Parkings = () => {
 
 export default Parkings;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: "#181818",
+  },
+  container: {
+    height: 220,
+    backgroundColor: "#202020",
+    borderBottomWidth: 1,
+    borderBottomEndRadius: 8,
+    borderBottomStartRadius: 8,
+    borderColor: "#2a2a2a",
+    paddingHorizontal: 22,
+    justifyContent: "center",
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 42,
+    fontFamily: "Inter_900Black",
+    textAlign: "center",
+  },
+});
