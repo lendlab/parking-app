cd5cbbd450a59678132d979312f4b8eec80623f5
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 08:14:09 -0300

    Create Map

---
+++ screens/Map.js
@@ -0,0 +1,63 @@
+import { useRoute } from "@react-navigation/native";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+const Map = () => {
+  const route = useRoute();
+  const [location, setLocation] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("No fueron dados los permisos");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      let address = await Location.reverseGeocodeAsync(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        },
+        {
+          useGoogleMaps: false,
+        }
+      );
+
+      setLocation(location);
+      setAddress(address[0]);
+    })();
+  }, []);
+
+  return (
+    <View style={{ width: "100%", height: "100%" }}>
+      <MapView
+        initialRegion={{
+          latitude: location?.coords?.latitude,
+          longitude: location?.coords?.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {location && address && (
+          <Marker
+            coordinate={{
+              latitude: location?.coords?.latitude,
+              longitude: location?.coords?.longitude,
+            }}
+            title="Tu"
+            description={`${address.street}, ${address.region}, ${address.city}, ${address.country}`}
+          ></Marker>
+        )}
+      </MapView>
+    </View>
+  );
+};
+
+export default Map;
