2d2b351ab1be25e5fb010dfd15bba32d86e52602
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 08:13:08 -0300

    Create graphql-queries

---
+++ graphql/auth/graphql-queries.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Mutation($options: LoginInput!) {
+    login(options: $options) {
+      errors {
+        path
+        message
+      }
+      user {
+        email
+        name
+      }
+    }
+  }
+`;
+
+export const ME = gql`
+  query Me {
+    me {
+      email
+      name
+    }
+  }
+`;
