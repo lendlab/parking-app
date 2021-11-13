f105cf79b919c2dbda05fc3bc05e72ea35e21ea6
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 08:12:13 -0300

    Add ApolloClient

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { StatusBar } from "react-native";
-import Login from "./screens/Login";
-import AppNavigator from "./navigators/AppNavigator";
+
 import AppLoading from "expo-app-loading";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
-const Stack = createStackNavigator();
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import AppNavigator from "./navigators/AppNavigator";
+
+const client = new ApolloClient({
+  uri: "https://parking-app-backend-ckjzi.ondigitalocean.app/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
-  const isLoggedIn = false;
   let [fontsLoaded] = useFonts({
     Inter_900Black,
   });
@@ -19,22 +21,10 @@ export default function App() {
     return <AppLoading />;
   } else {
     return (
-      <>
+      <ApolloProvider client={client}>
         <StatusBar backgroundColor="#181818" barStyle="light-content" />
-        <NavigationContainer>
-          {isLoggedIn ? (
-            <Stack.Navigator
-              screenOptions={{
-                headerShown: false,
-              }}
-            >
-              <Stack.Screen name="Login" component={Login} />
-            </Stack.Navigator>
-          ) : (
-            <AppNavigator />
-          )}
-        </NavigationContainer>
-      </>
+        <AppNavigator />
+      </ApolloProvider>
     );
   }
 }
