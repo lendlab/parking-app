641260c55b22d6f8db33ca434e2dc0bd577e7887
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 08:14:00 -0300

    Add formik and login mutation

--- screens/Login.js
+++ screens/Login.js
@@ -1,57 +1,92 @@
+import { Formik } from "formik";
 import React from "react";
-import { StyleSheet, Platform, StatusBar, View } from "react-native";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { Text } from "react-native-elements";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import { useLogin } from "../graphql/auth/custom-hooks";
 
 const Login = () => {
+  const [login, { loading, data, error }] = useLogin();
+
   return (
     <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-      <View style={{ paddingTop: 26 }}>
-        <Text
-          style={{ color: "#fff" }}
-          h1Style={{
-            fontSize: 42,
-          }}
-          h1
-        >
-          Vamos a iniciar sesión ahora
-        </Text>
-        <Text
-          style={{ color: "#fff" }}
-          h2
-          h2Style={{
-            fontWeight: "normal",
-            fontSize: 22,
-          }}
-        >
-          Bienvenido otra vez a Parking, te echabamos de menos
-        </Text>
-        <View style={{ paddingTop: 32 }}>
-          <Input label="Email" placeholder="Ingresa tu email" />
-          <Input label="Contraseña" placeholder="Ingresa tu contraseña" />
-        </View>
-      </View>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values) => {
+          return login({
+            variables: {
+              options: values,
+            },
+          });
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <>
+            <View style={{ paddingTop: 26 }}>
+              <Text
+                style={{ color: "#fff" }}
+                h1Style={{
+                  fontSize: 42,
+                }}
+                h1
+              >
+                Vamos a iniciar sesión ahora
+              </Text>
+              <Text
+                style={{ color: "#fff" }}
+                h2
+                h2Style={{
+                  fontWeight: "normal",
+                  fontSize: 22,
+                }}
+              >
+                Bienvenido otra vez a Parking, te echabamos de menos
+              </Text>
+              <View style={{ paddingTop: 32 }}>
+                <Input
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  value={values.email}
+                  label="Email"
+                  placeholder="Ingresa tu email"
+                />
+                <Input
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  value={values.password}
+                  label="Contraseña"
+                  placeholder="Ingresa tu contraseña"
+                />
+              </View>
+            </View>
 
-      <View style={{ paddingBottom: 26 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            width: "100%",
-            justifyContent: "center",
-            paddingBottom: 12,
-          }}
-        >
-          <Text style={{ marginRight: 4, color: "#e0e0e0" }}>
-            ¿No tienes cuenta?
-          </Text>
-          <Text style={{ color: "#e0e0e0", fontWeight: "bold" }}>
-            Registrate ahora
-          </Text>
-        </View>
-        <Button title="Iniciar sesión"></Button>
-      </View>
+            <View style={{ paddingBottom: 26 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  width: "100%",
+                  justifyContent: "center",
+                  paddingBottom: 12,
+                }}
+              >
+                <Text style={{ marginRight: 4, color: "#e0e0e0" }}>
+                  ¿No tienes cuenta?
+                </Text>
+                <Text style={{ color: "#e0e0e0", fontWeight: "bold" }}>
+                  Registrate ahora
+                </Text>
+              </View>
+              <Button
+                onPress={handleSubmit}
+                title="Submit"
+                title="Iniciar sesión"
+              ></Button>
+            </View>
+          </>
+        )}
+      </Formik>
     </KeyboardAwareScrollView>
   );
 };
