9d8231d390edccd1e17fcde0e8642ae8f7aff114
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 21:47:07 -0300

    Remove login

--- graphql/auth/graphql-queries.js
+++ graphql/auth/graphql-queries.js
@@ -1,20 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN = gql`
-  mutation Mutation($options: LoginInput!) {
-    login(options: $options) {
-      errors {
-        path
-        message
-      }
-      user {
-        email
-        name
-      }
-    }
-  }
-`;
-
 export const ME = gql`
   query Me {
     me {
