e2ea6bd9c0e470a3eca12522aa5f1a1ebf572801
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-13 21:49:42 -0300

    Create Reserve

---
+++ screens/Reserve.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Reserve = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Reserve;
