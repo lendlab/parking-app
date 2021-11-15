080496fc8d5dc0b624d979703b33fa3f7fd4ae38
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-15 03:26:00 -0300

    Create Timer

---
+++ screens/Timer.js
@@ -0,0 +1,79 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React from "react";
+import { Alert, SafeAreaView, View } from "react-native";
+import { Text } from "react-native-elements";
+import Button from "../components/Button";
+
+const Timer = () => {
+  const navigation = useNavigation();
+
+  const route = useRoute();
+
+  const { data } = route.params;
+
+  React.useEffect(
+    () =>
+      navigation.addListener("beforeRemove", (e) => {
+        e.preventDefault();
+
+        Alert.alert("Espera a que la reserva termine");
+      }),
+    [navigation]
+  );
+
+  return (
+    <>
+      <SafeAreaView backgroundColor="#181818"></SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#181818",
+          paddingHorizontal: 22,
+          justifyContent: "space-between",
+          paddingVertical: 26,
+        }}
+      >
+        <View>
+          <Text h1 h1Style={{ color: "white", fontWeight: "bold" }}>
+            Tienes una reserva activa
+          </Text>
+          <Text
+            h2
+            h2Style={{
+              color: "#fff",
+              fontSize: 18,
+            }}
+          >
+            Del lugar #{data.place.place_number}
+          </Text>
+        </View>
+        <View>
+          <Text
+            h2
+            h2Style={{
+              color: "#fff",
+              fontSize: 22,
+            }}
+          >
+            Esperando que llegues...
+          </Text>
+          <Text
+            h2
+            h2Style={{
+              color: "#fff",
+              fontSize: 22,
+              marginTop: 12,
+              fontWeight: "bold",
+            }}
+          >
+            Tu token es: {data.reservation_token}
+          </Text>
+        </View>
+
+        <Button title="¿Como llegar?"></Button>
+      </View>
+    </>
+  );
+};
+
+export default Timer;
