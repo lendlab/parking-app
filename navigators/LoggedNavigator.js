3ef7de711aabc528591a7f91a870ffe08024a5ea
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-15 03:24:54 -0300

    Show stacks conditionally if the user has an active reservation

--- navigators/LoggedNavigator.js
+++ navigators/LoggedNavigator.js
@@ -3,61 +3,91 @@ import {
   createStackNavigator,
   HeaderBackButton,
 } from "@react-navigation/stack";
+import AppLoading from "expo-app-loading";
 import React from "react";
+import { useActiveReservation } from "../graphql/reservations/custom-hooks";
+import Countdown from "../screens/Countdown";
 import Map from "../screens/Map";
 import Reserve from "../screens/Reserve";
+import Timer from "../screens/Timer";
 import TabNavigator from "./TabNavigator";
 
 const Stack = createStackNavigator();
 
 const LoggedNavigator = () => {
-  const navigation = useNavigation();
+  const { loading, data } = useActiveReservation();
+
+  if (loading || !data) return <AppLoading />;
+
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="App"
-        component={TabNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Map"
-        component={Map}
-        options={({ route }) => ({
-          title: route.params.title,
-          headerTitleStyle: {
-            color: "#fff",
-          },
-          headerBackTitle: null,
-          headerTintColor: "#ffffff",
-          headerStyle: {
-            height: 75,
-            backgroundColor: "#202020",
-          },
-        })}
-      />
-      <Stack.Screen
-        name="Reserve"
-        component={Reserve}
-        options={{
-          title: "Reservar",
-          headerTitleStyle: {
-            color: "#fff",
-            fontSize: 42,
-          },
-          headerBackTitle: null,
-          headerTintColor: "#ffffff",
-          headerStyle: {
-            height: 220,
-            backgroundColor: "#202020",
-            borderBottomWidth: 1,
-            borderBottomColor: "#2a2a2a",
-            elevation: 0,
-            shadowOpacity: 0,
-          },
-        }}
-      />
+      {data?.getMyActiveReservations ? (
+        <>
+          <Stack.Screen
+            name="Timer"
+            component={Timer}
+            options={() => ({
+              headerShown: false,
+            })}
+            initialParams={{ data: data.getMyActiveReservations }}
+          />
+          {/* <Stack.Screen
+            name="CountDown"
+            component={Countdown}
+            options={() => ({
+              headerShown: false,
+            })}
+            initialParams={{ data: data.getMyActiveReservations }}
+          /> */}
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="App"
+            component={TabNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Map"
+            component={Map}
+            options={({ route }) => ({
+              title: route.params.title,
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerBackTitle: null,
+              headerTintColor: "#ffffff",
+              headerStyle: {
+                height: 75,
+                backgroundColor: "#202020",
+                elevation: 0,
+                shadowOpacity: 0,
+              },
+            })}
+          />
+          <Stack.Screen
+            name="Reserve"
+            component={Reserve}
+            options={({ route }) => ({
+              title: route.params.placeName,
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerBackTitle: null,
+              headerTintColor: "#ffffff",
+              headerStyle: {
+                height: 75,
+                backgroundColor: "#202020",
+                elevation: 0,
+                shadowOpacity: 0,
+              },
+              headerBackTitle: " ",
+            })}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
