b6f85dd5f53f71db6b637c8478f1a98c7d391276
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-15 03:22:35 -0300

    Add ActivityIndicator

--- components/ParkingList.js
+++ components/ParkingList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import { Text } from "react-native-elements";
 import { useParkings } from "../graphql/parkings/custom-hooks";
 import ParkItem from "./ParkItem";
@@ -7,7 +7,7 @@ import ParkItem from "./ParkItem";
 const ParkingList = () => {
   const { loading, data, error } = useParkings();
 
-  if (loading || !data) return <Text>Cargando...</Text>;
+  if (loading || !data) return <ActivityIndicator color="#fff" />;
 
   return (
     <View style={{ paddingHorizontal: 22, paddingTop: 22 }}>
