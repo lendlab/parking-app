77397b1b850b8edc8a3951759831aaaff4cdc91c
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-15 03:23:04 -0300

    Add disabled when reserved

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -1,15 +1,22 @@
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { Chip, ListItem } from "react-native-elements";
 
 const PlaceItem = ({ place, actionSheet }) => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  console.log(place);
 
   return (
     <ListItem
       onPress={() => {
-        navigation.navigate("Reserve");
+        navigation.navigate("Reserve", {
+          placeId: place.place_id,
+          parkingId: route.params.id,
+          placeName: `Lugar ${place.place_id}`,
+        });
         actionSheet.current?.setModalVisible();
       }}
       bottomDivider
@@ -18,7 +25,7 @@ const PlaceItem = ({ place, actionSheet }) => {
         borderBottomColor: "#2a2a2a",
         borderBottomWidth: 1,
       }}
-      disabled={place.occuped}
+      disabled={place.occuped || place.state == "Solicitado"}
       disabledStyle={{
         opacity: 0.7,
       }}
@@ -33,12 +40,22 @@ const PlaceItem = ({ place, actionSheet }) => {
         <ListItem.Title style={{ color: "#fff", marginRight: 12 }}>
           Lugar #{place.place_id}
         </ListItem.Title>
-        <Chip
-          buttonStyle={{
-            backgroundColor: place.occuped ? "red" : "green",
-          }}
-          title={place.occuped ? "Ocupado" : "Libre"}
-        />
+
+        {place.state == "Solicitado" ? (
+          <Chip
+            buttonStyle={{
+              backgroundColor: "red",
+            }}
+            title="Solicitado"
+          />
+        ) : (
+          <Chip
+            buttonStyle={{
+              backgroundColor: place.occuped ? "red" : "green",
+            }}
+            title={place.occuped ? "Ocupado" : "Libre"}
+          />
+        )}
       </ListItem.Content>
       <ListItem.Chevron />
     </ListItem>
